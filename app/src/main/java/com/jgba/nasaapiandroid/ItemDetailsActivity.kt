ccbe822a50e61b66205531e459a750c2f5112f7a
gabriel7771 <juangax100@gmail.com>
2020-01-17 12:40:43 -0500

    Recyclerview is now selectable and show details for each list item

---
+++ app/src/main/java/com/jgba/nasaapiandroid/ItemDetailsActivity.kt
@@ -0,0 +1,26 @@
+package com.jgba.nasaapiandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_item_details.*
+
+class ItemDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_details)
+
+        val imageLink = intent.getStringExtra("ImageLink")
+        val title = intent.getStringExtra("Title")
+        val secondaryCreator = intent.getStringExtra("SecondaryCreator")
+        val dateCreated = intent.getStringExtra("DateCreated")
+        val description = intent.getStringExtra("Description")
+
+        Picasso.get().load(imageLink).into(imageView)
+        titleText.text = "$title"
+        secondaryCreatorText.text = "$secondaryCreator"
+        dateText.text = "$dateCreated"
+        descriptionText.text = "$description"
+    }
+}
--- app/src/main/java/com/jgba/nasaapiandroid/MyAdapter.kt
+++ app/src/main/java/com/jgba/nasaapiandroid/MyAdapter.kt
@@ -1,9 +1,11 @@
 package com.jgba.nasaapiandroid
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.jgba.nasaapiandroid.model.Item
 import com.squareup.picasso.Picasso
@@ -26,13 +28,28 @@ class MyAdapter(private val itemList: MutableList<Item>) : RecyclerView.Adapter<
         val imageText = holder.itemView.image_text
         val nasaImage = holder.itemView.nasa_image
 
+        val secondaryCreator = "${item.data[0].secondaryCreator}"
+        val dateCreated = "${item.data[0].dateCreated}"
+        val description = "${item.data[0].description}"
+        val imageLink = "${item.links[0].href}"
+
         val title = "${item.data[0].title}"
         imageText.text = title
 
-        Picasso.get().load(item.links[0].href).into(nasaImage)
+        Picasso.get().load(imageLink).into(nasaImage)
 
         holder.itemView.setOnClickListener{
             Toast.makeText(context, title, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, ItemDetailsActivity::class.java)
+            //Sending detailed info
+            intent.putExtra("ImageLink", imageLink)
+            intent.putExtra("Title", title)
+            intent.putExtra("SecondaryCreator",secondaryCreator)
+            intent.putExtra("DateCreated",dateCreated)
+            intent.putExtra("Description",description)
+            context.startActivity(intent)
+
         }
     }
 }
