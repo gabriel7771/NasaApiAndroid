b594edb82f0cf63ddb983f168bf8c2e9a3b4d965
gabriel7771 <juangax100@gmail.com>
2020-01-29 16:48:42 -0500

    Added progress bar to the searches.

--- app/src/main/java/com/jgba/nasaapiandroid/RecyclerViewActivity.kt
+++ app/src/main/java/com/jgba/nasaapiandroid/RecyclerViewActivity.kt
@@ -1,11 +1,15 @@
 package com.jgba.nasaapiandroid
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.core.view.size
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -25,18 +29,24 @@ class RecyclerViewActivity : AppCompatActivity() {
     private lateinit var myAdapter: SearchAdapter
 
     lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var progressBar: ProgressBar
+
+    lateinit var freeSearch: String
+    private var loaded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
 
         //Receive the freeSearch variable from MainActivity
-        val freeSearch = intent.getStringExtra("FreeSearch")
+        freeSearch = intent.getStringExtra("FreeSearch")
 
         this.title = """${getString(R.string.results_of_search)} $freeSearch"""
         val toolbar = findViewById(R.id.toolbar_favourite) as androidx.appcompat.widget.Toolbar?
         setSupportActionBar(toolbar)
         toolbar?.subtitle = getString(R.string.search_subtitle)
+
+        progressBar = findViewById(R.id.progressBar) as ProgressBar
 
         bottomNavigationView = findViewById(R.id.bottomNavigation)
         setIconChecked()
@@ -70,6 +80,8 @@ class RecyclerViewActivity : AppCompatActivity() {
         recycler_view.addItemDecoration(DividerItemDecoration(this, OrientationHelper.VERTICAL))
         recycler_view.adapter = myAdapter
 
+        httpRequest()
+        /*
         //Networking --> request de HTTP
         AndroidNetworking.initialize(this)
         //API root and the parameter q which allows free search based on it input String
@@ -78,19 +90,49 @@ class RecyclerViewActivity : AppCompatActivity() {
             .getAsObject( ModelJSON::class.java, object : ParsedRequestListener<ModelJSON>{
                 override fun onResponse(response: ModelJSON) {
                     //Add items objects from json response to itemList
+                    recycler_view.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
                     itemList.addAll(response.collection.items)
                     myAdapter.notifyDataSetChanged()
                 }
 
                 override fun onError(anError: ANError?) {
                     Log.d("ERROR : ", anError.toString())
+                    Toast.makeText(applicationContext, resources.getString(R.string.no_internet), Toast.LENGTH_LONG).show()
                 }
             })
-
+*/
     }
     override fun onResume() {
         super.onResume()
         setIconChecked()
+
+        if (!loaded){
+            httpRequest()
+        }
+    }
+    private fun httpRequest(){
+        //Networking --> request de HTTP
+        AndroidNetworking.initialize(this)
+        //API root and the parameter q which allows free search based on it input String
+        AndroidNetworking.get("https://images-api.nasa.gov/search?q=$freeSearch&media_type=image")
+            .build()
+            .getAsObject( ModelJSON::class.java, object : ParsedRequestListener<ModelJSON>{
+                override fun onResponse(response: ModelJSON) {
+                    //Add items objects from json response to itemList
+                    recycler_view.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+                    itemList.addAll(response.collection.items)
+                    myAdapter.notifyDataSetChanged()
+                    loaded = true
+                }
+
+                override fun onError(anError: ANError?) {
+                    Log.d("ERROR : ", anError.toString())
+                    Toast.makeText(applicationContext, resources.getString(R.string.no_internet), Toast.LENGTH_LONG).show()
+                    loaded = false
+                }
+            })
     }
     private fun setIconChecked(){
         val menu: Menu = bottomNavigationView.menu
